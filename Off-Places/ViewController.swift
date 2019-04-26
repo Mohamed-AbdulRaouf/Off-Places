98ef27acb9ea5e2dab5de4d5eb1cfb2acbc1acd4
esraa <esraa.moustafa.9@gmail.com>
2019-04-26 13:48:03 +0200

    Initial Commit

---
+++ Off-Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Off-Places
+//
+//  Created by Esraa on 4/26/19.
+//  Copyright © 2019 example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Off-PlacesTests/Off_PlacesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Off_PlacesTests.swift
+//  Off-PlacesTests
+//
+//  Created by Esraa on 4/26/19.
+//  Copyright © 2019 example. All rights reserved.
+//
+
+import XCTest
+@testable import Off_Places
+
+class Off_PlacesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
